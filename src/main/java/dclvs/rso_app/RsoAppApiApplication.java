4fb0a1e22d691687897fbaec06de896ae8274989
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-09-03 00:02:43 +0300

    create project by spring boot init

---
+++ src/main/java/dclvs/rso_app/RsoAppApiApplication.java
@@ -0,0 +1,13 @@
+package dclvs.rso_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsoAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsoAppApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dclvs/rso_app/RsoAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dclvs.rso_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsoAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
