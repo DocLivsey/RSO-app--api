c4815f8dc486b786cba4aa113cdf2751f55d28f5
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-10-19 02:00:51 +0300

    feat: make data receive class from http

---
+++ src/main/java/dclvs/rso_app/entities/EventReceiver.java
@@ -0,0 +1,17 @@
+package dclvs.rso_app.entities;
+
+public class EventReceiver {
+
+    private Long eventId;
+
+    private String tagName;
+
+    private String description;
+
+    private String scheduledDate;
+
+    private String beginAt;
+
+    private String endsAt;
+
+}
