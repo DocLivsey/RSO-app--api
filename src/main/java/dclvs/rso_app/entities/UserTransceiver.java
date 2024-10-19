39b60bbf83f9ffe6da390735738411a2e8516a17
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-10-19 12:46:33 +0300

    feat: code classes base fields and methods

--- src/main/java/dclvs/rso_app/entities/UserTransceiver.java
+++ src/main/java/dclvs/rso_app/entities/UserTransceiver.java
@@ -1,4 +1,39 @@
 package dclvs.rso_app.entities;
 
+import dclvs.rso_app.entities.components.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.Calendar;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
 public class UserTransceiver {
+
+    private Long userId;
+
+    private Email email;
+
+    private Password password;
+
+    private String firstName;
+
+    private String surName;
+
+    private String patronymicName;
+
+    private Calendar birthday;
+
+    private Faculty faculty;
+
+    private Speciality speciality;
+
+    private Integer course;
+
+    private Group group;
+
 }
