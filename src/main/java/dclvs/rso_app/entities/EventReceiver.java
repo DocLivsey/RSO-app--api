989399617afb9bccc6b21ec591a14825c551a3b8
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-10-19 12:31:09 +0300

    feat: add some annotations for generating class's basic methods

--- src/main/java/dclvs/rso_app/entities/EventReceiver.java
+++ src/main/java/dclvs/rso_app/entities/EventReceiver.java
@@ -1,5 +1,14 @@
 package dclvs.rso_app.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
 public class EventReceiver {
 
     private Long eventId;
