3ad0cc99140ff6b7489a911c737e6b53a8c2143b
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-10-23 02:26:00 +0300

    build: remove hashing methods, in temporal case compare passwords with equals + hashcode

--- src/main/java/dclvs/rso_app/entities/components/Password.java
+++ src/main/java/dclvs/rso_app/entities/components/Password.java
@@ -2,40 +2,23 @@ package dclvs.rso_app.entities.components;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 @Data
-@AllArgsConstructor
-public class Password {
+@Slf4j
+@EqualsAndHashCode
+public class Password { // TODO: in future create password hashing -> as consequence supplement password datasource mapping model
 
     private String password;
 
-    // TODO: this method of hashing has been found not to comply with the determinism requirement
-    public byte[] mocPasswordHashing() throws NoSuchAlgorithmException {
-        // Generating salt (key)
-        SecureRandom secureRandom = new SecureRandom();
-        byte[] salt = new byte[16];
-        secureRandom.nextBytes(salt);
-
-        // Hashing password
-        MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
-        messageDigest.update(salt);
-        return messageDigest.digest(password.getBytes());
-    }
-
-    public boolean matches(Password password) {
-        return hashCode() == password.hashCode();
-    }
-
-    public boolean matches(String password) {
-        return matches(new Password(password));
-    }
-
-    public boolean matches(int password) {
-        return hashCode() == password;
+    public Password(String password) {
+        this.password = password;
+        this.password = STR."\{hashCode()}";
     }
 
 }
