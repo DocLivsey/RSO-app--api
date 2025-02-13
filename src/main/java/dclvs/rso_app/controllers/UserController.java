022fea32ba0f6f416d7fd7a28322120484436148
DocLivsey <doclivsey@github.com>
2025-02-13 15:08:14 +0300

    feat: added spring annotation to mark classes as bean-restcontrollers

--- src/main/java/dclvs/rso_app/controllers/UserController.java
+++ src/main/java/dclvs/rso_app/controllers/UserController.java
@@ -1,4 +1,13 @@
 package dclvs.rso_app.controllers;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("/user")
 public class UserController {
 }
