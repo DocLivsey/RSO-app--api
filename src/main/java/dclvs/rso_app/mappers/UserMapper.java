779eb786044eb5939b4886b8a52ee701904fb7e0
DocLivsey <doclivsey@github.com>
2025-02-13 14:45:59 +0300

    feat: completed mapper for convert data from transceiver to receiver classes objects

--- src/main/java/dclvs/rso_app/mappers/UserMapper.java
+++ src/main/java/dclvs/rso_app/mappers/UserMapper.java
@@ -1,7 +1,64 @@
 package dclvs.rso_app.mappers;
 
+import dclvs.rso_app.entities.UserReceiver;
+import dclvs.rso_app.entities.UserTransceiver;
+import dclvs.rso_app.entities.components.*;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.Named;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 @Mapper
 public interface UserMapper {
+
+    @Mappings({
+            @Mapping(source = "userId", target = "userId"),
+            @Mapping(source = "email", target = "email", qualifiedByName = "mapEmail"),
+            @Mapping(source = "password", target = "password", qualifiedByName = "mapPassword"),
+            @Mapping(source = "firstName", target = "firstName"),
+            @Mapping(source = "surName", target = "surName"),
+            @Mapping(source = "patronymicName", target = "patronymicName"),
+            @Mapping(source = "birthday", target = "birthday", qualifiedByName = "mapBirthday"),
+            @Mapping(source = "faculty", target = "faculty", qualifiedByName = "mapFaculty"),
+            @Mapping(source = "speciality", target = "speciality", qualifiedByName = "mapSpeciality"),
+            @Mapping(source = "course", target = "course"),
+            @Mapping(source = "group", target = "group", qualifiedByName = "mapGroup")
+    })
+    UserReceiver toReceiver(UserTransceiver userTransceiver);
+
+    @Named("mapEmail")
+    default String map(Email email) {
+        return email.getEmail();
+    }
+
+    @Named("mapPassword")
+    default String map(Password password) {
+        return password.getPassword();
+    }
+
+    // TODO: check in database how birthday was wrote in table
+    @Named("mapBirthday")
+    default String map(Calendar birthday) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        return simpleDateFormat.format(birthday.getTime());
+    }
+
+    @Named("mapFaculty")
+    default String map(Faculty faculty) {
+        return faculty.getTitleRUS();
+    }
+
+    @Named("mapSpeciality")
+    default String map(Speciality speciality) {
+        return speciality.getTitleRUS();
+    }
+
+    @Named("mapGroup")
+    default String map(Group group) {
+        return group.getTitleRUS();
+    }
+
 }
